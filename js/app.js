f30e0faa84882838dc8d9aca70c7a865561c4835
NicDominguez <NicDominguez@users.noreply.github.com>
2019-07-18 11:46:21 -0700

    final code adjustements

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 
 const keyboard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const startGameButton = document.getElementsByClassName('btn__reset');
+let startGameButton = document.getElementsByClassName('btn__reset')[0];
 const overlay = document.getElementById("overlay");
 let matchingLetters = []
 let chosenKeys = []
@@ -55,8 +55,8 @@ const addPhraseToDisplay = (characterArray) => {
 
 //checks if button pressed is included in object of elements with class "letter"
 const checkLetter = (button) => {
-    const buttonText = button.innerText;
-    const letters = document.getElementsByClassName("letter");
+    let buttonText = button.innerText;
+    let letters = document.getElementsByClassName("letter");
     for (i = 0; i < letters.length; i++) {
         if (buttonText === letters[i].innerText.toLowerCase()) {
             letters[i].classList.add('show')
@@ -75,34 +75,28 @@ const checkWin = () => {
     const letters = document.getElementsByClassName("letter");
     const lettersShown = document.getElementsByClassName("show");
     const title = overlay.firstElementChild;
+    
     if (letters.length === lettersShown.length) {
         overlay.classList.remove("start")
         overlay.classList.add("win")
-        overlay.style.visibility = "visible";
+        overlay.style.visibility = "visible"
         title.innerText= "You Win!"
-        startGameButton.innerText = "Play Again?"
+        startGameButton.innerText = "Shall we play again?"
+
     } else if (missed >= 5) {
         overlay.classList.remove("start")
         overlay.classList.add("lose")
-        overlay.style.visibility = "visible";
+        overlay.style.visibility = "visible"
         title.innerText = "Sorry, you lost =("
-        startGameButton.innerText = "Play Again?"
+        startGameButton.innerText = "Shall we play again?"
     }
 }
 
-/////////////////
-//Events
-/////////////////
-
-//removes overlay when start game butten clicked
-startGameButton[0].addEventListener("click", function (e) {
-    //hides overlay
-    e.target.parentNode.style.visibility = "hidden";
-    //chooses and displays new phrase
+const resetGame = () => {
+    console.log("reset function running")
     phraseArray = getRandomPhraseAsArray(phrases)
     addPhraseToDisplay(phraseArray);
-
-    if (startGameButton.innerText === "Play Again?") {
+    if (startGameButton.innerHTML === "Shall we play again?") {
         console.log("if statement running")
         //resets lives
         missed = 0
@@ -123,17 +117,28 @@ startGameButton[0].addEventListener("click", function (e) {
         while (child) {
             ul.removeChild(child);
             child = ul.lastElementChild;
-        } 
+        }
 
         //chooses and displays new phrase
         phraseArray = getRandomPhraseAsArray(phrases)
-        console.log(phraseArray)
         addPhraseToDisplay(phraseArray);
 
         //remove los and win class from overlay
         overlay.classList.remove("lose")
         overlay.classList.remove("win")
     }
+}
+
+/////////////////
+//Events
+/////////////////
+
+//removes overlay when start game butten clicked
+startGameButton.addEventListener("click", (e) => {
+    //hides overlay
+    e.target.parentNode.style.visibility = "hidden";
+    //chooses and displays new phrase
+    resetGame()
 })
 
 //runs checkLetter function when keyboard button is clicked and adds classes to button
